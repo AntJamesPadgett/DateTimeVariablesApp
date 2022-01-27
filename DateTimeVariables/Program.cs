f0a476c75b84fdbd43b49bae2c428898be8b85e8
antjamespadgett <padgettanthonyj@gmail.com>
2022-01-27 15:33:44 -0600

    completed work

---
+++ DateTimeVariables/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+DateTime today = DateTime.Now;
+Console.WriteLine(today.ToString("t"));
+
+
+DateTime birthday = DateTime.Parse("9/21/1973");
+Console.WriteLine(birthday.ToString());
+
+
+
+
